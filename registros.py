4221152cd9480f04025624c43a7519547ef6c942
Felipe Arias <felipe2arias@hotmail.com>
2019-09-14 12:46:28 -0300

    Update registros.py

--- registros.py
+++ registros.py
@@ -8,10 +8,17 @@ class Publicacion:
         self. puntuacion = puntuacion
 
 def write(publicacion):
-    print("------------------------")
-    print("-Codigo de publicacion: ", publicacion.codigo)
-    print("-Precio: ", publicacion.precio)
-    print("-Ubicacion geografica: ", publicacion.ubicacion)
-    print("-Estado: ", publicacion.estado)
-    print("-Cantidad disponible: ", publicacion.cantidad)
-    print("-Puntuacion del vendedor: ", publicacion.puntuacion)
+    ubicaciones = "Buenos Aires", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa", "La Rioja", "Mendoza", "Misiones", "Neuquén"\
+        , "Río Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe", "Santiago del Estero", "Tierra del Fuego", "Tucumán"
+    estados = "Nuevo", "Usado"
+    calificaciones = "Mala", "Regular", "Buena", "Muy buena", "Excelente"
+    print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    renglon = ""
+    renglon += "Codigo: "'{:<10}'.format(publicacion.codigo)
+    renglon += "Precio: ""$" '{:<20}'.format(publicacion.precio)
+    renglon += "Ubicacion: "'{:>10}'.format('{:21.20}'.format(ubicaciones[publicacion.ubicacion-1]))
+    renglon += "Estado: "'{:<20}'.format(estados[publicacion.estado])
+    renglon += "Cantidad: "'{:<20}'.format(publicacion.cantidad)
+    renglon += "Puntuacion: "'{:<20}'.format(calificaciones[publicacion.puntuacion-1])
+    print(renglon)
+
